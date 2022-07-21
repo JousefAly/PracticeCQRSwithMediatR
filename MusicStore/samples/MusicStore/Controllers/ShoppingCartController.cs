e52d12a1d93e514da03a24fdb9c21c863c1e5116
JousefAly <youssefali00286@gmail.com>
2022-07-21 15:55:33 -0700

    Initial

---
+++ MusicStore/samples/MusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,98 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MusicStore.Features.ShoppingCart;
+using MusicStore.Models;
+using MusicStore.ViewModels;
+
+namespace MusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly ILogger<ShoppingCartController> _logger;
+        private readonly IMediator _mediator;
+
+        public ShoppingCartController(MusicStoreContext dbContext, ILogger<ShoppingCartController> logger, IMediator mediator)
+        {
+            DbContext = dbContext;
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        public MusicStoreContext DbContext { get; }
+
+        //
+        // GET: /ShoppingCart/
+        public async Task<IActionResult> Index()
+        {
+            var cart = ShoppingCart.GetCart(DbContext, HttpContext);
+
+            // Set up our ViewModel
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = await cart.GetCartItems(),
+                CartTotal = await cart.GetTotal()
+            };
+
+            // Return the view
+            return View(viewModel);
+        }
+
+        
+
+        //
+        // AJAX: /ShoppingCart/RemoveFromCart/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveFromCart(
+            int id,
+            CancellationToken requestAborted)
+        {
+            // Retrieve the current user's shopping cart
+            var cart = ShoppingCart.GetCart(DbContext, HttpContext);
+
+            // Get the name of the album to display confirmation
+            var cartItem = await DbContext.CartItems
+                .Where(item => item.CartItemId == id)
+                .Include(c => c.Album)
+                .SingleOrDefaultAsync();
+
+            string message;
+            int itemCount;
+            if (cartItem != null)
+            {
+                // Remove from cart
+                itemCount = cart.RemoveFromCart(id);
+
+                await DbContext.SaveChangesAsync(requestAborted);
+
+                string removed = (itemCount > 0) ? " 1 copy of " : string.Empty;
+                message = removed + cartItem.Album.Title + " has been removed from your shopping cart.";
+            }
+            else
+            {
+                itemCount = 0;
+                message = "Could not find this item, nothing has been removed from your shopping cart.";
+            }
+
+            // Display the confirmation message
+
+            var results = new ShoppingCartRemoveViewModel
+            {
+                Message = message,
+                CartTotal = await cart.GetTotal(),
+                CartCount = await cart.GetCount(),
+                ItemCount = itemCount,
+                DeleteId = id
+            };
+
+            _logger.LogInformation("Album {id} was removed from a cart.", id);
+
+            return Json(results);
+        }
+    }
+}
---
+++ MusicStore/samples/MusicStore/Features/ShoppingCart/AddToCart.cs
@@ -0,0 +1,99 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MusicStore.Models;
+
+namespace MusicStore.Features.ShoppingCart
+{
+
+    public class AddToCartController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public AddToCartController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("/ShoppingCart/AddToCart/{id:int}")]
+        public async Task<IActionResult> AddToCart(int id, CancellationToken requestAborted)
+        {
+            var cartId = Models.ShoppingCart.GetCartId(HttpContext);
+            await _mediator.SendAsync(new AddToCart(cartId, id), requestAborted);
+
+            // Go back to the main store page for more shopping
+            return RedirectToAction("Index", "ShoppingCart");
+        }
+    }
+
+    public class AddToCart : ICancellableAsyncRequest<Unit>
+    {
+        public string CartId { get; }
+        public int AlbumId { get; }
+
+        public AddToCart(string cartId, int albumId)
+        {
+            CartId = cartId;
+            AlbumId = albumId;
+        }
+    }
+
+    public class AddToCartHandler : ICancellableAsyncRequestHandler<AddToCart, Unit>
+    {
+        private readonly IMediator _mediator;
+        private readonly MusicStoreContext _dbContext;
+
+        public AddToCartHandler(IMediator mediator, MusicStoreContext dbContext)
+        {
+            _mediator = mediator;
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(AddToCart message, CancellationToken cancellationToken)
+        {
+            // Retrieve the album from the database
+            var addedAlbum = await _dbContext.Albums
+                .SingleAsync(album => album.AlbumId == message.AlbumId, cancellationToken);
+
+            // Add it to the shopping cart
+            var cart = Models.ShoppingCart.GetCart(_dbContext, message.CartId);
+
+            await cart.AddToCart(addedAlbum);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            _mediator.Publish(new AlbumAddedToCart(addedAlbum.AlbumId));
+
+            return Unit.Value;
+        }
+    }
+
+    public class AlbumAddedToCart : INotification
+    {
+        public int AlbumId { get; }
+
+        public AlbumAddedToCart(int albumId)
+        {
+            AlbumId = albumId;
+        }
+    }
+
+    public class AlbumAddedToCartHandler : INotificationHandler<AlbumAddedToCart>
+    {
+        private readonly ILogger<AddToCart> _logger;
+
+        public AlbumAddedToCartHandler(ILogger<AddToCart> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Handle(AlbumAddedToCart notification)
+        {
+            _logger.LogInformation("Album {albumId} was added to the cart.", notification.AlbumId);
+        }
+    }
+}
+
