486958037c55ac1b388a70dd75a5b0f656a01545
JousefAly <youssefali00286@gmail.com>
2022-07-24 12:14:05 -0700

    added project and gitignore

--- CQRSandMediatR/CQRSandMediatR/WeatherForecast.cs
+++ CQRSandMediatR/CQRSandMediatR/WeatherForecast.cs
@@ -1,4 +1,6 @@
-namespace ImplementCQRSandMediator
+using System;
+
+namespace CQRSandMediatR
 {
     public class WeatherForecast
     {
@@ -8,6 +10,6 @@ namespace ImplementCQRSandMediator
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
